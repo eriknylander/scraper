f9afc52e2e4e9fa6bac469734f246dd546a75332
Erik Nylander <nylander.e@gmail.com>
2023-10-02 22:24:33 +0200

    Add user input parameters

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 
 	"github.com/eriknylander/scraper/internal/filesystem"
@@ -10,10 +11,31 @@ import (
 	"github.com/eriknylander/scraper/internal/scraping"
 )
 
+var (
+	siteToScrape    = flag.String("s", "", "The site to scrape")
+	outputDirectory = flag.String("o", "", "Output directory")
+)
+
 func main() {
 	ctx := context.Background()
 
-	s := scraping.NewScraper("http://books.toscrape.com", "C:\\Users\\nylan\\go\\src\\github.com\\eriknylander\\scraper\\downloaded", http.NewFetcher(), links.NewHTMLParser(), filesystem.NewFileWriter())
+	flag.Parse()
+
+	if siteToScrape == nil || *siteToScrape == "" {
+		fmt.Println("Missing site to scrape argument")
+		flag.PrintDefaults()
+
+		return
+	}
+
+	if outputDirectory == nil || *outputDirectory == "" {
+		fmt.Println("Missing output directory argument, for usage, see scrape -h")
+		flag.PrintDefaults()
+
+		return
+	}
+
+	s := scraping.NewScraper(*siteToScrape, *outputDirectory, http.NewFetcher(), links.NewHTMLParser(), filesystem.NewFileWriter())
 
 	downloaded, err := s.Scrape(ctx)
 	if err != nil {
